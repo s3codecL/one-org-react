50312b1c966725f18351df107c580f9e5ee34975
s3code <gandradev@gmail.com>
2024-07-14 22:43:46 -0400

    add btn change color

--- src/componentes/Equipo/index.js
+++ src/componentes/Equipo/index.js
@@ -1,14 +1,16 @@
 import "./Equipo.css"
 import Colaborador from "../Colaborador"
+import hexToRgba from 'hex-to-rgba';
+
 
 const Equipo = (props) => {
     //Destructuracion
-    const { colorPrimario, colorSecundario, titulo } = props.datos
-    const { colaboradores, eliminarColaborador } = props
+    const { colorPrimario, colorSecundario, titulo, id } = props.datos
+    const { colaboradores, eliminarColaborador, actualizarColor } = props
     const obj = {
-        backgroundColor: colorSecundario
+        backgroundColor: hexToRgba(colorPrimario, 0.6)
     }
-    console.log(colaboradores.length > 0)
+
 
     const estiloTitulo = { borderColor: colorPrimario }
 
@@ -16,6 +18,14 @@ const Equipo = (props) => {
         {
             colaboradores.length > 0 &&
             <section className="equipo" style={obj}>
+                <input
+                    type='color'
+                    className="input-color"
+                    value={colorPrimario}
+                    onChange={(evento) => {
+                        actualizarColor(evento.target.value, id)
+                    }}
+                />
                 <h3 style={estiloTitulo} >{titulo}</h3>
                 <div className="colaboradores">
                     {
